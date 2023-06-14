ba66f1c9144e2a4cd121ceb43cf09f05ff135a4d
a-reznikov <a.reznnikov@gmail.com>
2023-06-14 18:05:57 +0300

    feat: implement solution to 05-objects-tasks.js whithout css builder

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -19,8 +19,16 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  return {
+    width: this.width,
+    height: this.height,
+    getArea() {
+      return width * height;
+    },
+  };
 }
 
 /**
@@ -33,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -48,8 +56,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const objFromJson = JSON.parse(json);
+  Object.setPrototypeOf(objFromJson, proto);
+  return (objFromJson);
 }
 
 /**
@@ -107,7 +117,7 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
+  element(/* value) {
     throw new Error('Not implemented');
   },
 
